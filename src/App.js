5c37c8412f7d3275210a127a67076f606a996ff1
Serigne_Sonhibou <sonhibou298@gmail.com>
2022-06-23 09:40:15 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header>
+        <div className="row">
+            <div className="bienvenue">
+                <p>Bienvenue sur la plateform Dig-Dajeg</p>
+            </div>
+            <div className="info">
+                <span className="phone-icon"><i className="fa fa-phone"></i> 33 855 64 28</span>
+                <span className="date-icon"><i className="fa fa-calendar-plus-o"></i> 08:00 - 22:00 (Lun-Sam)</span>
+                <span className="email-icon"><i className="fa fa-envelope-o"></i> <a href="#">info@healthcenter.com</a></span>
+            </div>
+        </div>
+      </Header>
   );
 }
-
+const Header = styled.header`
+  margin: 0px;
+  padding: 0px;
+  .row{
+    display: flex;
+    
+  }
+    p{
+      color: #757575;
+    }
+  
+  span{
+    font-weight: 500;
+    display: inline-block;
+    padding: 11px 15px;
+  }
+`
 export default App;
